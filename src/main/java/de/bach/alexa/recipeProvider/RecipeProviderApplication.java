118e9297d32b49ffdc38fc573597e355f7c3be94
christian <christianbach1689@googlemail.com>
2024-02-02 13:56:49 +0100

    initial commit

---
+++ src/main/java/de/bach/alexa/recipeProvider/RecipeProviderApplication.java
@@ -0,0 +1,13 @@
+package de.bach.alexa.recipeProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/bach/alexa/recipeProvider/RecipeProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bach.alexa.recipeProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
